bfce201fa0da89768c210b7d2b0439c888674f28
Mads Mikkel Rasmussen <mara@aspit.dk>
2021-03-17 13:00:27 +0100

    ...

--- WpfItemsControls/ViewModel.cs
+++ WpfItemsControls/ViewModel.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WpfItemsControls
 {
-    class ViewModel
+    class ViewModel: INotifyPropertyChanged
     {
+        private Person selectedPerson;
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
         public ViewModel()
         {
             Persons = new ObservableCollection<Person>(GetPersons());
         }
 
         public ObservableCollection<Person> Persons { get; set; }
+        public Person SelectedPerson
+        {
+            get => selectedPerson;
+            set
+            {
+                selectedPerson = value;
+                OnPropertyChanged(nameof(SelectedPerson));
+            }
+        }
+
 
         public List<Person> GetPersons()
         {
